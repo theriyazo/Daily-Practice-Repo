2bde37c06d42ac5fba2def4a231245b96f7e5c4e
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2021-12-30 21:16:16 +0530

    strict mode in jS

---
+++ JavaScript/📜 Scripts/Day2 - Fundamentals (Part 1)/strictMode.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if(passTest) hasDriverLicense = true;
+if(hasDriverLicense) console.log("You can Drive 😉!");
+
+// Canot use the reserved keywords as variable
+const private = 10;
