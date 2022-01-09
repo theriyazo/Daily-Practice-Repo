bb52d9f9651dd3d57678653db21c9ee088bc3ac9
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2022-01-09 20:18:05 +0530

    Optimized code and reset button

--- JavaScript/Projects/Day7 - Pig Game/script.js
+++ JavaScript/Projects/Day7 - Pig Game/script.js
@@ -1,25 +1,75 @@
 'use strict';
 
 // Selecting Elements
+const fullBody = document.querySelector('body');
+
 const player0EL = document.querySelector('.player--0');
 const player1EL = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-const currentEl1 = document.querySelector('#current--0');
-const currentEl2 = document.querySelector('#current--1');
+
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+const nameEl0 = document.querySelector('#name--0');
+const nameEl1 = document.querySelector('#name--1');
+
+const label0El = document.getElementById('label--0');
+const label1El = document.getElementById('label--1');
+
+const curWinner0El = document.querySelector('.current--winner0');
+const curWinner1El = document.querySelector('.current--winner1');
 
 // Starting conditions
 let scores = [0, 0];
 let activePlayer = 0;
 let playing = true;
-score0El.textContent = 0;
-score1El.textContent = 0;
 let currnetScore = 0;
-diceEl.classList.add('hide'); // hide dice at first
+
+// Resetting to starting conditions
+const init = function () {
+  scores = [0, 0];
+
+  activePlayer = 0;
+  currnetScore = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  playing = true;
+
+  fullBody.style.backgroundImage =
+    'linear-gradient(to top left, #88ECDF 0%, #74E9AE 100%)';
+  diceEl.classList.add('hide'); // hide dice at first
+
+  player0EL.classList.add('player--active');
+  player1EL.classList.remove('player--active');
+  player0EL.classList.remove('player--winner');
+  player1EL.classList.remove('player--winner');
+
+  nameEl0.textContent = 'PLAYER 1';
+  nameEl1.textContent = 'PLAYER 2';
+
+  score0El.style.color = '#39DFCA';
+  score1El.style.color = '#39DFCA';
+
+  curWinner0El.style.backgroundColor = '#39DFCA';
+  curWinner1El.style.backgroundColor = '#39DFCA';
+
+  current0El.style.color = '#FFFFFF';
+  current1El.style.color = '#FFFFFF';
+  label0El.style.color = '#FFFFFF';
+  label1El.style.color = '#FFFFFF';
+};
+
+init();
 
 // Generating Random Number for Randowm dice Roll
 const DiceRoll = function () {
@@ -29,10 +79,15 @@ const DiceRoll = function () {
   return dice;
 };
 
+// Switching player by swaping 0 <-> 1
 const switchPlayerAndResetScore = function () {
+  // resetting current score to 0
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currnetScore = 0;
+
+  // Swaping active player
   activePlayer = activePlayer === 0 ? 1 : 0;
+
   player0EL.classList.toggle('player--active');
   player1EL.classList.toggle('player--active');
 };
@@ -50,7 +105,6 @@ btnRoll.addEventListener('click', function () {
       document.getElementById(`current--${activePlayer}`).textContent =
         currnetScore;
     } else {
-      //   Reset privious player score to zero and display the same
       // switch to next player
       switchPlayerAndResetScore();
     }
@@ -67,7 +121,7 @@ btnHold.addEventListener('click', function () {
     // Switch player and reset Values
 
     // Check if players score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       //   Finish Game
       document
@@ -87,17 +141,13 @@ btnHold.addEventListener('click', function () {
         `.current--winner${activePlayer}`
       ).style.backgroundColor = '#FFFFFF';
 
-      document.querySelector(`.current--winner${activePlayer}`).style.color =
-        '#444444';
-
-      document.querySelector(`body`).style.backgroundImage =
+      fullBody.style.backgroundImage =
         'linear-gradient(to top left, #74E9AE 0%, #39DF8B 100%)';
 
       document.getElementById(`current--${activePlayer}`).textContent = 0;
       document.getElementById(`current--${activePlayer}`).textContent = 0;
       document.getElementById(`current--${activePlayer}`).style.color =
         '#13d271';
-
       console.log(`label--${activePlayer}`);
 
       document.getElementById(`label--${activePlayer}`).style.color = '#13d271';
@@ -108,37 +158,6 @@ btnHold.addEventListener('click', function () {
 });
 
 btnNew.addEventListener('click', function () {
-  player0EL.classList.add('player--active');
-  player1EL.classList.remove('player--active');
-
-  scores = [0, 0];
-  document
-    .querySelector(`.player--${activePlayer}`)
-    .classList.remove('player--winner');
-
-  document.querySelector(`#name--${activePlayer}`).textContent = `PLAYER ${
-    activePlayer + 1
-  }`;
-
-  document.querySelector(`#score--${activePlayer}`).style.color = '#39DFCA';
-
-  document.querySelector(
-    `.current--winner${activePlayer}`
-  ).style.backgroundColor = '#39DFCA';
-
-  document.getElementById(`current--${activePlayer}`).style.color = '#FFFFFF';
-  document.getElementById(`label--${activePlayer}`).style.color = '#FFFFFF';
-
-  document.getElementById(`current--${activePlayer}`).textContent = 0;
-  activePlayer = 0;
-  playing = true;
-  currnetScore = 0;
-
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-
-  document.querySelector(`body`).style.backgroundImage =
-    'linear-gradient(to top left, #88ECDF 0%, #74E9AE 100%)';
-
-  diceEl.classList.add('hide'); // hide dice at first
+  // ResetEverything and start a new game
+  init();
 });
