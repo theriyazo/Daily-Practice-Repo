2400f860f97aacec8774c507968e3556ce2e2d3b
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2022-01-15 15:35:57 +0530

    removed extra code

--- JavaScript/📜 Scripts/Day13 - DS & Modern Operators/loopFor-Of.js
+++ JavaScript/📜 Scripts/Day13 - DS & Modern Operators/loopFor-Of.js
@@ -6,26 +6,6 @@ const restaurant = {
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
-
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  orderPasta: function (ing1, ing2, ing3) {
-    console.log(
-      `You\'re Order of pasta with ${ing1}, ${ing2} and ${ing3} is received!`
-    );
-  },
 };
 
 const fullMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
