148abdddd3b948fb97258eb348066de8ffaa8ab1
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2022-02-05 20:07:40 +0530

    closures and classes

--- JavaScript/📜 Scripts/Day19 - Promises/closures.js
+++ JavaScript/📜 Scripts/Day19 - Promises/closures.js
@@ -1,12 +1,56 @@
-function x() {
-  const a = 10;
-  return function y() {
-    console.log(a);
+// function x() {
+//   const a = 10;
+//   return function y() {
+//     console.log(a);
+//   };
+// }
+
+// const result = x();
+// // function x() doesn't exists in the call stack
+// setTimeout(function () {
+//   result();
+// }, 3000);
+
+// Delete below code
+
+// Objects
+class anObject {
+  firstName = "myName";
+  lastName = "abc";
+  phnNo = 7204987524;
+  calcAge = (year) => {
+    this.currentYear = 2022;
+    return currentYear - year;
+  };
+  print(namee) {
+    return `My Name is ${namee}`;
   };
 }
 
-const result = x();
-// function x() doesn't exists in the call stack
-setTimeout(function () {
-  result();
-}, 3000);
+// Class in JavaScript
+// function anObjOfObj() {
+//   this.firstName = "RiyazAhamad";
+//   (this.lastName = "Hosalli"),
+//     (this.phnNo = 7406564326),
+//     (this.calcAge = (year) => {
+//       this.currentYear = 2022;
+//       return currentYear - year;
+//     });
+// }
+
+// Inheritance
+class anotherObj extends anObject {
+  print(namee){
+    return super.print(namee);
+  };
+ 
+}
+
+// const riyaz = new anObjOfObj();
+
+// console.log(riyaz);
+
+
+const riyaz = new anotherObj();
+console.log(riyaz.print('riyaz'));
+
