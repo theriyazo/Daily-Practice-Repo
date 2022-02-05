6fa7c74e63aadd012dff0be2f13a60c91fd082f8
RiyazAhamad <55573156+theriyazo@users.noreply.github.com>
2022-02-05 20:29:34 +0530

    got to know about default parameters today

--- JavaScript/📜 Scripts/Day20 - Functions/defaultParameter.js
+++ JavaScript/📜 Scripts/Day20 - Functions/defaultParameter.js
@@ -1,1 +1,27 @@
-'use strict';
+"use strict";
+
+const bookings = [];
+
+const createBookings = function (
+  flightNum = 0,
+  numOfPassenger = 0,
+  
+  // default parameters can be expressions and can use other parameters value as well, the only condition is that that parameter has to be pre defined as parameter
+  price = 150 * numOfPassenger
+) {
+  const booking = {
+    flightNum,
+    numOfPassenger,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+// Without default paremeters
+createBookings(10);
+// {flightNum: 10, numOfPassenger: undefined, price: undefined}
+
+// With default parameters
+createBookings(10, 20);
+// {flightNum: 10, numOfPassenger: 20, price: 3000}
